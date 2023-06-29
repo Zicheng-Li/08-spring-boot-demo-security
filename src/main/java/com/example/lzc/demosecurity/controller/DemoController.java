7dd1ce4e16337f36a9d256f115f1b336c7bb50cb
ZL&EL <lzc123456ab@gmail.com>
2023-06-29 00:12:41 -0400

    06

--- src/main/java/com/example/lzc/demosecurity/controller/DemoController.java
+++ src/main/java/com/example/lzc/demosecurity/controller/DemoController.java
@@ -17,4 +17,10 @@ public class DemoController {
     public String showLeaders() {
         return "leaders";
 
-}}
+}
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
+}
--- src/main/java/com/example/lzc/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/example/lzc/demosecurity/security/DemoSecurityConfig.java
@@ -48,7 +48,6 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll())
-
         .logout(logout ->logout.permitAll() );
         return http.build();
     }
